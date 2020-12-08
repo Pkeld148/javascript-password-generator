257610692d577c41e6802ecd7076d4cfb5692dd6
Paul Keldsen <Pkeld148@gmail.com>
2020-12-08 17:34:09 -0500

    added arrays for lowercase letters, uppercase letters, numbers, and special characters

--- script.js
+++ script.js
@@ -10,9 +10,18 @@ function writePassword() {
 
 }
 
+const lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+const upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+const numbers = [0,1,2,3,4,5,6,7,8,9];
+const specialCharacters = ["!","#","$","%","&","(",")","*","+","-",".","/",":",";","<","?","@","[","]","^","_","`","{","|","}","~","]"];
+
+
 function generatePassword(){
 
   // ALL THE LOGIC GOES HERE DUMB ASS
+
+
+
 
   return "temporary password";
 }
