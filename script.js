383717abb35692b3b7ba33379590d7d9a0fc15de
Paul Keldsen <Pkeld148@gmail.com>
2020-12-09 16:45:15 -0500

    added function to combine all selected characters into one new array, included all 4 types in password generation

--- script.js
+++ script.js
@@ -10,11 +10,11 @@ function writePassword() {
 
 }
 
-const lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-const upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-const numbers = [0,1,2,3,4,5,6,7,8,9];
-const specialCharacters = ["!","#","$","%","&","(",")","*","+","-",".","/",":",";","<","?","@","[","]","^","_","`","{","|","}","~","]"];
-var finalPassword = [];
+var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numbers = [0,1,2,3,4,5,6,7,8,9];
+var specialCharacters = ["!","#","$","%","&","(",")","*","+","-",".","/",":",";","<","?","@","[","]","^","_","`","{","|","}","~","]"];
+
 
 
 function getRandomInt(max) {
@@ -24,26 +24,43 @@ function getRandomInt(max) {
 
 
 function generatePassword(){
+ 
+  var allChosenCharacters = [];
+  var finalPassword = [];
 
-  characterLength = prompt("How many characters long would you like your password to be? \nMinimum: 8 Digits \nMaximum: 16 Digits");
+  characterLength = prompt("How many characters long would you like your password to be? \nMinimum: 8 Digits \nMaximum: 128 Digits");
   console.log(characterLength);
 
-    if (characterLength < 8 || characterLength > 16) {
-      return "Please choose password length between 8 and 16 digits! \nPress the button to try again!";
+    if (characterLength < 8 || characterLength > 128) {
+      return "Please choose password length between 8 and 128 digits! \nPress the button to try again!";
     }
 
   confirmLowerCase = confirm("Would you like to have lowercase letters as part of your password?");
+  if (confirmLowerCase) {
+    allChosenCharacters = allChosenCharacters.concat(lowerCase);
+  }
 
   confirmUpperCase = confirm("Would you like to have UPPERCASE letters as part of your password?");
+  if (confirmUpperCase) {
+    allChosenCharacters = allChosenCharacters.concat(upperCase);
+  }
 
   confirmNumbers = confirm("Would you like to have numbers as part of your password?");
+  if (confirmNumbers) {
+    allChosenCharacters = allChosenCharacters.concat(numbers);
+  }
 
   confirmSpecialCharacters = confirm("Would you like to have special characters as part of your password? \nExamples: !'*+,-./:;<=>?@^_`{|}~");
+  if (confirmSpecialCharacters) {
+   allChosenCharacters = allChosenCharacters.concat(specialCharacters);
+  }
 
+  console.log(allChosenCharacters);
+ 
 
  for (i = 0 ; i < characterLength ; i++) {
-   var rand = getRandomInt(lowerCase.length);
-  finalPassword.push(lowerCase[rand])
+   var rand = getRandomInt(allChosenCharacters.length);
+  finalPassword.push(allChosenCharacters[rand])
  }
 
 
